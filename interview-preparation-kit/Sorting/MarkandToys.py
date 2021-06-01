6919c5e91eaec385513debf4f677ed17d60292c3
nakano0518 <m15sc018.prog@gmail.com>
2021-06-01 14:23:07 +0000

    [Add] Sorting question solved

---
+++ interview-preparation-kit/Sorting/MarkandToys.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maximumToys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY prices
+#  2. INTEGER k
+#
+
+def maximumToys(prices, k):
+    # Write your code here
+    cnt = 0
+    money = k 
+    while money > 0:
+        money = money - min(prices)
+        prices.remove(min(prices)) 
+        cnt += 1
+    return cnt-1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()

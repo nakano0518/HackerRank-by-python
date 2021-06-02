ecd7328fc9eaa0a084f9c517e19dd37d5c739e09
nakano0518 <m15sc018.prog@gmail.com>
2021-06-02 09:56:50 +0000

    [Add] StringManipulation question solved

---
+++ interview-preparation-kit/StringManipulation/Strings:MakingAnagrams.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+#
+# Complete the 'makeAnagram' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. STRING a
+#  2. STRING b
+#
+
+def makeAnagram(a, b):
+    # Write your code here
+    ct_a = Counter(a)
+    ct_b = Counter(b)
+    ct_a.subtract(ct_b)
+    return sum(abs(i) for i in ct_a.values())
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = input()
+
+    b = input()
+
+    res = makeAnagram(a, b)
+
+    fptr.write(str(res) + '\n')
+
+    fptr.close()
